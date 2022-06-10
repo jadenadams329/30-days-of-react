8011a625733e890b2c87cca36c8237d84203a64d
jadenadams329 <jadenadams329@gmail.com>
2022-06-10 11:57:09 -0700

    Day 3 exercises complete

--- Day_3/my-first-app/src/index.js
+++ Day_3/my-first-app/src/index.js
@@ -3,7 +3,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import techImage from "./images/frontend_technologies.png";
-
+import profilePhoto from "./images/feature-react.webp";
+import check from "./images/circle-check-solid.svg";
 // JSX element, header
 const header = (
   <header>
@@ -17,6 +18,37 @@ const techStack = (
     <img src={techImage} />
   </div>
 );
+
+const person = {
+  firstName: "Jaden",
+  lastName: "Adams",
+  devLevel: "Noob Developer",
+  country: "USA",
+  skills: [
+    "HTML",
+    "CSS",
+    "JS",
+    "React",
+    "Angular",
+    "Node",
+    "Git",
+    "ASP",
+    "API",
+    "C#",
+    "SQE",
+  ],
+  photo: (
+    <div>
+      <img src={profilePhoto} />
+    </div>
+  ),
+};
+
+const checkMark = <img className="check" src={check} />;
+
+const skillsFormated = person.skills.map((skill) => <li>{skill}</li>);
+
+const date = new Date(2022, 6, 10);
 
 // JSX element, main
 const main = (
@@ -62,6 +94,34 @@ const main = (
             <input className="button" type="submit" value="Subscribe" />
           </div>
         </form>
+      </div>
+      <div>
+        <h2>Exersice Level 4</h2>
+      </div>
+      <div className="background">
+        <div className="levelFourWrapper">
+          <div className="profilePhoto">{person.photo}</div>
+          <div className="person">
+            <h3>
+              {person.firstName} {person.lastName}
+            </h3>
+            {checkMark}
+          </div>
+          <div>
+            <p>
+              {person.devLevel}, {person.country}
+            </p>
+          </div>
+          <div>
+            <h3>Skills</h3>
+          </div>
+          <div className="pill">{skillsFormated}</div>
+          <div className="joinedWrapper">
+            <p>
+              <i class="fa fa-clock-o"></i> Joined on June 10, 2022
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
